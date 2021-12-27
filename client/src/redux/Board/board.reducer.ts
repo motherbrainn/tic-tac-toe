3cba5ef6d2c1cc6ee134523aed354d3ab4808ab7
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2021-12-27 00:29:10 -0500

    add rules for win

--- client/src/redux/Board/board.reducer.ts
+++ client/src/redux/Board/board.reducer.ts
@@ -15,9 +15,9 @@ const reducer = (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
     case SET_GAME_BOARD_STATE:
       const copyOfBoardState = [...state.boardState];
-      //checkForWinner()
       copyOfBoardState[action.payload.rowId][action.payload.columnId] =
         action.payload.socketId;
+      checkForWinner(state.playerState, state.boardState);
       return { ...state, boardState: copyOfBoardState };
     case CREATE_PLAYER:
       return {
--- client/src/utils/utilityFunctions.ts
+++ client/src/utils/utilityFunctions.ts
@@ -12,6 +12,88 @@ export const turnDecider = (currentPlayerId: number, playerArray: number[]) => {
 };
 
 export const checkForWinner = (playerArray: Player[], boardState) => {
-  console.log("playerArray: ", playerArray);
-  console.log("boardState: ", boardState);
+  console.log(boardState);
+  //horizontal wins
+  if (
+    boardState[0][0] !== 0 &&
+    boardState[0][1] !== 0 &&
+    boardState[0][2] !== 0 &&
+    boardState[0][0] === boardState[0][1] &&
+    boardState[0][0] === boardState[0][2]
+  ) {
+    console.log("win: ");
+    return boardState[0][0];
+  }
+  if (
+    boardState[1][0] !== 0 &&
+    boardState[1][1] !== 0 &&
+    boardState[1][2] !== 0 &&
+    boardState[1][0] === boardState[1][1] &&
+    boardState[1][0] === boardState[1][2]
+  ) {
+    console.log("win: ");
+    return boardState[0][0];
+  }
+  if (
+    boardState[2][0] !== 0 &&
+    boardState[2][1] !== 0 &&
+    boardState[2][2] !== 0 &&
+    boardState[2][0] === boardState[2][1] &&
+    boardState[2][0] === boardState[2][2]
+  ) {
+    console.log("win: ");
+    return boardState[0][0];
+  }
+  //vertical wins
+  if (
+    boardState[0][0] !== 0 &&
+    boardState[1][0] !== 0 &&
+    boardState[2][0] !== 0 &&
+    boardState[0][0] === boardState[1][0] &&
+    boardState[0][0] === boardState[2][0]
+  ) {
+    console.log("win: ");
+    return boardState[0][0];
+  }
+  if (
+    boardState[0][1] !== 0 &&
+    boardState[1][1] !== 0 &&
+    boardState[2][1] !== 0 &&
+    boardState[0][1] === boardState[1][1] &&
+    boardState[0][1] === boardState[2][1]
+  ) {
+    console.log("win: ");
+    return boardState[0][1];
+  }
+  if (
+    boardState[0][2] !== 0 &&
+    boardState[1][2] !== 0 &&
+    boardState[2][2] !== 0 &&
+    boardState[0][2] === boardState[1][2] &&
+    boardState[0][2] === boardState[2][2]
+  ) {
+    console.log("win: ");
+    return boardState[0][2];
+  }
+  //diagonal wins
+  if (
+    boardState[0][0] !== 0 &&
+    boardState[1][1] !== 0 &&
+    boardState[2][2] !== 0 &&
+    boardState[0][0] === boardState[1][1] &&
+    boardState[0][0] === boardState[2][2]
+  ) {
+    console.log("win: ");
+    return boardState[0][0];
+  }
+  if (
+    boardState[0][2] !== 0 &&
+    boardState[1][1] !== 0 &&
+    boardState[2][0] !== 0 &&
+    boardState[0][2] === boardState[1][1] &&
+    boardState[0][2] === boardState[2][0]
+  ) {
+    console.log("win: ");
+    return boardState[0][2];
+  }
 };
