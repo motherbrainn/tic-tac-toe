5b2b9ff4d636dabbffedfb73e2dae06e73929a8a
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-01-02 20:00:32 -0800

    zz

--- client/src/connection/socket.ts
+++ client/src/connection/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8080");
+//export const socket = io("http://localhost:8080");
 
-//export const socket = io("https://nft-tic-tac-toe.herokuapp.com");
+export const socket = io("https://nft-tic-tac-toe.herokuapp.com");
