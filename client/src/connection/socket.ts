cb8b88f2824dc509754207548438f8e5b20eee70
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-01-02 21:08:05 -0800

    zz

--- client/src/connection/socket.ts
+++ client/src/connection/socket.ts
@@ -1,13 +1,6 @@
 import { io } from "socket.io-client";
 
-const env = process.env.NODE_ENV;
+//export const socket = io("http://localhost:8080");
+console.log(process.env.NODE_ENV);
 
-const determineClientSideAddress = (env: string) => {
-  if (env === "production") {
-    return "https://nft-tic-tac-toe.herokuapp.com";
-  } else {
-    return "http://localhost:8080";
-  }
-};
-
-export const socket = io(determineClientSideAddress(env));
+export const socket = io("https://nft-tic-tac-toe.herokuapp.com");
